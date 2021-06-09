42b510ccabd6ef5c56e94659362af9812e3d9700
Philip Ziolkowski <philipz4848@gmail.com>
2021-06-09 12:49:21 -0700

    Adding styling for header and linked css file in html file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const baseURL = "https://kitsu.io/api/edge/anime"
+/* const baseURL = "https://kitsu.io/api/edge/anime"
 
 
 fetch(baseURL)
 .then(resp => resp.json())
 .then(resp => {
     console.log(resp)
-})
+}) */
