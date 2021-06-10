37536517f0e1d6e3f7d5fab4c8dbece3c88e8333
Philip Ziolkowski <philipz4848@gmail.com>
2021-06-10 10:09:15 -0700

    Swtiching API, adding dropdowns

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-/* const baseURL = "https://kitsu.io/api/edge/anime"
+const baseURL = "http://www.boredapi.com/api/activity"
 
 
-fetch(baseURL)
+const resultsContainer = document.getElementById('search-results-container')
+
+
+/* fetch(baseURL)
 .then(resp => resp.json())
 .then(resp => {
     console.log(resp)
+  const pTag = document.createElement('p')
+   pTag.innerText = `${resp.links.first}`
+   resultsContainer.appendChild(pTag) 
 }) */
